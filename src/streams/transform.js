6df5d0217f8a64cb82dea2e9b222d09fe628171e
joelans <baileyderek59@gmail.com>
2025-10-26 20:23:58 +0300

    feat: implement streams/transform function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,17 @@
+import { Transform } from 'node:stream';
+import { pipeline } from 'node:stream/promises';
+
 const transform = async () => {
   // Write your code here
+  const transformStream = new Transform({
+    transform(chunk, encoding, callback) {
+      const reversed = chunk.toString().split('').reverse().join('');
+      this.push(reversed);
+      callback();
+    }
+  });
+
+  await pipeline(process.stdin, transformStream, process.stdout);
 };
 
 await transform();
