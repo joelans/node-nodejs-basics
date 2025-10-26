79079cec36cae600964ba3579b9de3ac472bf005
joelans <baileyderek59@gmail.com>
2025-10-26 17:37:51 +0300

    feat: implement hash/calcHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import { createReadStream } from 'node:fs';
+import { join } from 'node:path';
+import { createHash } from 'node:crypto';
+import { pipeline } from 'node:stream/promises'
+
 const calculateHash = async () => {
   // Write your code here
+  const hash = createHash('sha256');
+  const filePath = join(import.meta.dirname, 'files', 'fileToCalculateHashFor.txt');
+  const readStream = createReadStream(filePath, 'utf8');
+
+  await pipeline(readStream, hash);
+  console.log(hash.digest('hex'));
 };
 
 await calculateHash();
