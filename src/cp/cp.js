333c2c67bda6ca06b43c25292fa5b39a9ce11949
joelans <baileyderek59@gmail.com>
2025-10-27 01:43:41 +0300

    feat: implement cp function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,24 @@
+import { join } from 'node:path';
+import { spawn } from 'child_process';
+
 const spawnChildProcess = async (args) => {
   // Write your code here
+  const filePath = join(import.meta.dirname, 'files', 'script.js');
+
+  const childProcess = spawn('node', [filePath, ...args], {
+    stdio: ['pipe', 'pipe']
+  });
+
+  process.stdin.pipe(childProcess.stdin);
+  childProcess.stdout.pipe(process.stdout);
+
+  childProcess.on('exit', code => {
+    process.exit(code);
+  });
+
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+// spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+
+spawnChildProcess(['first', 'second']);
