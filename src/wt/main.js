eae8fca4bb88d19bf1eefacd954351a77133446b
joelans <baileyderek59@gmail.com>
2025-10-26 23:38:47 +0300

    feat: implement wt/worker and wt/main functions

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,34 @@
+import { join } from 'node:path';
+import { Worker } from 'worker_threads';
+import { availableParallelism } from 'node:os';
+
 const performCalculations = async () => {
   // Write your code here
+  const cores = availableParallelism();
+
+  const tasks = [];
+
+  for (let i = 0; i < cores; i++) {
+    const task = new Promise(resolve => {
+      const worker = new Worker(join(import.meta.dirname, 'worker.js'));
+      worker.postMessage(10 + i);
+
+      worker.on('message', result => {
+        resolve({ status: 'resolved', data: result });
+        worker.terminate();
+      });
+
+      worker.on('error', () => {
+        resolve({ status: 'error', data: null });
+        worker.terminate();
+      });
+    });
+
+    tasks.push(task);
+  }
+
+  const result = await Promise.all(tasks);
+  console.log(result);
 };
 
 await performCalculations();
