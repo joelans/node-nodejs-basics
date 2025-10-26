35eceae07b2fef557c93dc6f0531b67ef8a07aba
joelans <baileyderek59@gmail.com>
2025-10-26 14:06:17 +0300

    feat: implement fs/list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,21 @@
+import fs from 'node:fs/promises';
+import { join } from 'path';
+
 const list = async () => {
   // Write your code here
+  try {
+    const directoryPath = join(import.meta.dirname, 'files');
+    const files = await fs.readdir(directoryPath, { withFileTypes: true });
+    const filenames = [];
+    for (const file of files) {
+      if (file.isFile()) {
+        filenames.push(file.name);
+      }
+    }
+    console.log(filenames);
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await list();
